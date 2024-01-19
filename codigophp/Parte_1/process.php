4531aff2c50d64b2781f811852e4d374b3378d63
Patricio <pcifrey501@g.educaand.es>
2024-01-19 20:39:46 +0100

    show errors implemented

--- codigophp/Parte_1/process.php
+++ codigophp/Parte_1/process.php
@@ -1,11 +1,9 @@
 <?php
 
-if ($_SERVER["REQUEST_METHOD"] === "POST"){
+function checkQuestions($q)
+{
 
-    for ($i=0; $i<count($_POST); $i++) {
+    return $_SERVER["REQUEST_METHOD"] == "POST" && empty($_POST[$q]);
 
-        $isAnswered = isset($_POST["q" . $i]);
-
-        if (!$isAnswered) echo "<p>You must answer all questions!</p>";
-    }
 }
+?>
