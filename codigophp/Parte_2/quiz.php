e5fbbd6eae06b00e9e7ba204f740c95d530dbc03
Patricio <pcifrey501@g.educaand.es>
2024-01-23 20:29:59 +0100

    show questionary from database

--- codigophp/Parte_2/quiz.php
+++ codigophp/Parte_2/quiz.php
@@ -17,16 +17,22 @@ include 'QuizClass.php';
 
     <?php
 
-    $conection = mysqli_connect("db", "user", "user", "quizz-app");
+    $connection = mysqli_connect("db", "user", "user", "quizz-app");
 
-    $sql = "select * from questions";
-    $resultado = mysqli_query($conection, $sql);
-    if (mysqli_num_rows($resultado) > 0){
-        echo "pepeeeeee";
-    }
-    mysqli_close($conection);
+        $sql = "select * from questions";
+        $result = mysqli_query($connection, $sql);
+    while ($row = mysqli_fetch_assoc($result)) {
     ?>
-
+    <article class="question">
+        <p><?= $row['question_text'] ?></p>
+        <label><input type="radio" name="question_<?= $row['question_id'] ?>" value="a" <?php echo (isset($selectedOptions["question_" . $row['question_id']]) && $selectedOptions["question_" . $row['question_id']] === 'a') ? 'checked' : ''; ?>><?= $row['option_a'] ?></label>
+        <label><input type="radio" name="question_<?= $row['question_id'] ?>" value="b" <?php echo (isset($selectedOptions["question_" . $row['question_id']]) && $selectedOptions["question_" . $row['question_id']] === 'b') ? 'checked' : ''; ?>><?= $row['option_b'] ?></label>
+        <label><input type="radio" name="question_<?= $row['question_id'] ?>" value="c" <?php echo (isset($selectedOptions["question_" . $row['question_id']]) && $selectedOptions["question_" . $row['question_id']] === 'c') ? 'checked' : ''; ?>><?= $row['option_c'] ?></label>
+    </article>
+    <?php
+    }
+    mysqli_close($connection);
+    ?>
     <input type="submit" value="Submit">
 </form>
 
