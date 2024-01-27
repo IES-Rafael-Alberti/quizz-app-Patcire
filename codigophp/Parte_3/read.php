c91fca236ef2a592c6664940fd9f6572a980c15a
Patricio <pcifrey501@g.educaand.es>
2024-01-27 18:18:35 +0100

    read from CRUD implemented

--- codigophp/Parte_3/read.php
+++ codigophp/Parte_3/read.php
@@ -1,1 +1,21 @@
 <?php
+    include 'QuizClassP3.php';
+    $quiz = new QuizClassP3(1);
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Read questions</title>
+    <link rel="stylesheet" href="quizP3.css">
+</head>
+<body>
+    <a class="again" href="index.html">Menu</a>
+    <h1>read questions</h1>
+    <?php $quiz->showOnlyquestions();?>
+</body>
+</html>
